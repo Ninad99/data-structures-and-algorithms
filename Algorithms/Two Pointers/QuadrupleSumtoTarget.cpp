bcfe80b5860f35d35bab3b15326635c560fb1caf
ninad99 <ninuch7@gmail.com>
2020-08-14 15:41:27 +0530

    Quadruple Sum to Target

--- Algorithms/Two Pointers/QuadrupleSumtoTarget.cpp
+++ Algorithms/Two Pointers/QuadrupleSumtoTarget.cpp
@@ -15,5 +15,63 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+void searchPair(vector<int>& nums, int targetSum, int first, int second, vector<vector<int>>& result) {
+  int left = second + 1;
+  int right = nums.size() - 1;
+  while (left < right) {
+    int sum = nums[first] + nums[second] + nums[left] + nums[right];
+    if (sum == targetSum) {
+      result.push_back({ nums[first], nums[second], nums[left], nums[right] });
+      left++;
+      right--;
+      while (left < right && nums[left] == nums[left - 1]) left++;
+      while (left < right && nums[right] == nums[right + 1]) right--;
+    } else if (sum < targetSum) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+}
+
+vector<vector<int>> searchQuadruplets(vector<int>& nums, int target) {
+  sort(nums.begin(), nums.end());
+  vector<vector<int>> quadruplets;
+  int n = nums.size(), left, right;
+  for (int i = 0; i < n - 3; i++) {
+    if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+    for (int j = i + 1; j < n - 2; j++) {
+      if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+      searchPair(nums, target, i, j, quadruplets);
+    }
+  }
+
+  return quadruplets;
+}
+
+void print(vector<vector<int>>& grid) {
+  for (int i = 0; i < grid.size(); i++) {
+    for (int j = 0; j < grid[i].size(); j++) {
+      cout << grid[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+  vector<int> arr = {4, 1, 2, -1, 1, -3};
+  vector<vector<int>> result = searchQuadruplets(arr, 1);
+  print(result);
+
+  arr = {2, 0, -1, 1, -2, 2};
+  result = searchQuadruplets(arr, 2);
+  print(result);
+
+  return 0;
+}
